bf63b0086ab2707e19aa31ca551fd77c5f5a3813
Olman Alonso Acuña Quesada <61434202+OlmanAQ@users.noreply.github.com>
2023-11-15 13:51:27 -0600

    forms base

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,149 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState, } from 'react'
 
 export default function Home() {
+  const [formContent, setFormContent] = useState([]);
+  const [onEdit, setOnEdit] = useState(false);
+  const [textField, setTextField] = useState("");
+  const [editedField, setEditedField] = useState("");
+
+  const addQuestion = () => {
+    const field = {
+      "name": `question_${formContent.length}`,
+      "label": "Untitled question",
+      "question_type": "short_answer", // "paragraph", "multichoice",
+      "list": []
+    }
+    setFormContent([...formContent, field]);
+  }
+
+  const editField = (fieldName, fieldLabel) => {
+    const formFields = [...formContent];
+    const fieldIndex = formFields.findIndex(f => f.name === fieldName);
+    if (fieldIndex > -1){
+      formFields[fieldIndex].label = fieldLabel;
+      setFormContent(formFields);
+    }
+  }
+
+  const editFieldType = (fieldName, fieldLabel) => {
+    const formFields = [...formContent];
+    const fieldIndex = formFields.findIndex(f => f.name === fieldName);
+    if (fieldIndex > -1){
+      formFields[fieldIndex].question_type = fieldLabel;
+      setFormContent(formFields);
+    }
+  }
+
+  const addFieldOption =  (fieldName, option) => {
+    const formFields = [...formContent];
+    const fieldIndex = formFields.findIndex(f => f.name === fieldName);
+    if (fieldIndex > -1){
+      if (option && option != ""){
+        formFields[fieldIndex].list.push(option);
+        setFormContent(formFields);
+        setTextField("");
+      }
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='container mx-auto px-4 h-screen'>
+      <div className='flex flex-col w-full space-y-2 my-4'>
+        <h1 className='text-2xl font-bold'>Form Maker</h1>
+        <h2 className='text-lg'>Untitled Form</h2>
+      </div>
+      <div className='bg-white shadow-lg rounded-md p-5 my-10'>
+        {
+          formContent.map((field) => {
+            return (
+              <div key={field.name}>
+                <div className='flex justify-between items-center space-y-2'>
+                  <div key={field.name} className="block text-sm font-medium text-gray-700 capitalize">
+                   {
+                    onEdit && (editedField === field.name) ?
+                    <input type="text" value={field.label} onChange={(e) => editField(field.name, e.target.value)} onBlur={() => {setOnEdit(false);setEditedField("")}} /> 
+                    :
+                     <label onClick={() => {setOnEdit(true); setEditedField(field.name)}}>{field.label}</label>
+                   }
+                  </div>
+                  <div>
+                    <select onChange={(e) => editFieldType(field.name, e.target.value)}>
+                      <option value="short_answer">Short Answer</option>
+                      <option value="paragraph">Paragraph</option>
+                      <option value="multichoice">Multichoice</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className='my-4'>
+                  {
+                    field.question_type == 'short_answer' && <input type="text" className="px-5 shadow-sm h-10 rounded-md block w-full" placeholder={field.label} />
+                  }
+                  {
+                    field.question_type == 'paragraph' && <textarea rows={4} className="px-5 shadow-sm h-10 rounded-md block w-full" placeholder={field.label} />
+                  }
+                  {field.question_type == 'multichoice' &&
+                    <div className='my-4 flex flex-col space-y-2'>
+                      <select
+                        className='px-5 shadow-sm h-10 rounded-md block w-full'>
+                        {field.list.map((item) => <option key={item} value={item}>{item}</option>)}
+                      </select>
+                      <div className='flex space-between'>
+                        <input type="text" onChange={(e) => setTextField(e.target.value)} value={textField} placeholder="Add an option" className='flex-1' />
+                        <button className='bg-indigo-700 block hover:bg-indigo-900 text-white px-4' onClick={() => addFieldOption(field.name, textField) }>Add</button>
+                      </div>
+                    </div>
+                  }
+                </div>
+
+              </div>
+            )
+          })
+        }
+
+        <div className='relative w-full p-5'>
+          <div className='absolute inset-x-0 bottom-0 h-12 flex justify-center'>
+            <button onClick={() => addQuestion()} className='inline-flex bg-gray-800 hover:bg-gray-700 items-center p-3 text-sm text-white rounded-md'>Add Question</button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex flex-col w-full space-y-2 my-4'>
+        <h1 className='text-2xl font-bold'>Form Maker Preview</h1>
+        <h2 className='text-lg'>Untitled Form</h2>
       </div>
+      <div className='bg-white shadow-lg rounded-md p-5 my-10'>
+        {
+          formContent.map((field) => {
+            return (
+              <div key={field.name}>
+                <div className='flex justify-between items-center space-y-2'>
+                  <div key={field.name} className="block text-sm font-medium text-gray-700 capitalize">
+                  <label onClick={() => setOnEdit(true)}>{field.label}</label>
+                  </div>
+                 
+                </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div className='my-4'>
+                  {
+                    field.question_type == 'short_answer' && <input type="text" className="px-5 shadow-sm h-10 rounded-md block w-full" placeholder={field.label} />
+                  }
+                  {
+                    field.question_type == 'paragraph' && <textarea rows={4} className="px-5 shadow-sm h-10 rounded-md block w-full" placeholder={field.label} />
+                  }
+                  {
+                    field.question_type == 'multichoice' &&
+                    <select
+                      className='px-5 shadow-sm h-10 rounded-md block w-full'>
+                      {field.list.map((item) => <option key={item} value={item}>{item}</option>)}
+                    </select>
+                  }
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              </div>
+            )
+          })
+        }
       </div>
-    </main>
+    </div>
   )
 }
